d7db50d5547ab63d1812590d40d2d98374bb930d
ZAII <ivy2846357@icloud.com>
2021-09-01 23:12:22 +0800

    電腦版頁面雛形

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,30 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
+//Initialize Swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
